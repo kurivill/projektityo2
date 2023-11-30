d24d95b927652985350b4c57de30d0b8a40da79d
kurivill <kurikkaville@hotmail.fi>
2023-11-30 16:24:14 +0200

    Alustavaa pythonin tekemistä

---
+++ python_ja_database/pythonia.py
@@ -0,0 +1,14 @@
+class pelaaja:
+    def __init__(self, nimi, aloitussijainti):
+        self.nimi = nimi
+        self.rahat = 1000
+        self.sijainti = aloitussijainti
+        self.lentokm = 0
+
+    """TÄhän sit ehkä metodi siirtymisestä, pitää suunnitella"""
+    """Voi olla että pitää myös tehdä metodi tietojen updateemiseen databaseen"""
+
+
+
+
+    
