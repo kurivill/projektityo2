f7c04344786802078e340c3f4f7e8bdaeb105535
kurivill <kurikkaville@hotmail.fi>
2023-12-10 18:25:41 +0200

    Korjattu hieman pelaaja- ja peli-olioita, jotta ne on helpompi tallettaa

--- python_ja_database/yhteys.py
+++ python_ja_database/yhteys.py
@@ -3,7 +3,7 @@ import mysql.connector
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port= 3306,
-    database='prokkis2',
+    database='projekti2',
     user='root',
     password='Relaatio23s1',
     autocommit=True
